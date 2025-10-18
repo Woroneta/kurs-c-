311cdd2ccf08dd95f1efba890f125851e41a83f7
Woroneta <WiktorWrobel816@gmail.com>
2025-10-18 16:29:53 +0200

    Create czy ujemna.cpp

---
+++ czy ujemna.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	double liczba;
+
+	cout << "podaj liczbe:";
+	cin >> liczba;
+	if (liczba > 0) {
+		cout << "liczba jest dodatnia";
+	}else{
+		cout << "liczba jest ujemna";
+	return 0;
+	}
+}
