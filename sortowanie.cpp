94316b36ee56e36115dd277e7da0e725c4a504bb
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 09:35:31 +0200

    Update sortowanie.cpp

--- sortowanie.cpp
+++ sortowanie.cpp
@@ -13,17 +13,23 @@ int main()
 	cin >> l2;
 	cout << "wpisz liczbe trzecia:";
 	cin >> l3;
-	if (l1 < l2 && l2 < l3)
-		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l1<<"<" << l2 << "<" << l3;
-	else if (l2 < l1 && l1 < l3)
-		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l2 << "<" << l1<<"<"<< l3;
-	else if (l3 < l1 && l1 < l2)
+	if (l1 < l2 && l2 < l3) {
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l1 << "<" << l2 << "<" << l3;
+	}
+	else if (l2 < l1 && l1 < l3) {
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l2 << "<" << l1 << "<" << l3;
+	}
+	else if (l3 < l1 && l1 < l2) {
 		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l3 << "<" << l1 << "<" << l2;
-	else if (l3 < l2 && l2 < l1)
-		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l3 << "<" <<l2 << "<" << l1;
-	else if (l2 < l3 && l3 < l1)
+	}
+	else if (l3 < l2 && l2 < l1) {
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l3 << "<" << l2 << "<" << l1;
+	}
+	else if (l2 < l3 && l3 < l1) {
 		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l2 << "<" << l3 << "<" << l1;
-	else
-		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l1 << "<" <<l3 << "<" << l2;
+	}
+	else {
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l1 << "<" << l3 << "<" << l2;
+	}
 	return 0;
 }
