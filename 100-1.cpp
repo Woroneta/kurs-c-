66489cd73e4b94784521ac25a6b63fe794e1098a
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 11:58:48 +0200

    Create 100-1.cpp

---
+++ 100-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	
+	for (int x = 100; x >= 1; --x) {
+		cout << x << endl;
+	}
+	
+	return 0;
+}
