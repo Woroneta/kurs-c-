945087023f94c8b5865d6dfd31fa32697295d68a
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 11:43:47 +0200

    Create liczenie.cpp

---
+++ liczenie.cpp
@@ -0,0 +1,16 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	int i;
+	for (int i = 1; i <= 10; i++) {
+		cout << i << endl;
+	}
+	
+
+	return 0;
+}
