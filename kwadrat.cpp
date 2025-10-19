f63cb594566ea15cf51552bb2f3881354e82245d
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 12:13:29 +0200

    Create kwadrat.cpp

---
+++ kwadrat.cpp
@@ -0,0 +1,17 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	
+	for (int x = 1,ilo=0; x <= 10; ++x) {
+		ilo= x*x;
+		cout << ilo << endl;
+	
+	}
+
+	return 0;
+}
