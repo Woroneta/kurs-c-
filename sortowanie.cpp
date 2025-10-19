53757e60dcc7074b91388730698ce85604f3460e
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 09:31:41 +0200

    Create sortowanie.cpp

---
+++ sortowanie.cpp
@@ -0,0 +1,29 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	double l1, l2, l3;
+	cout << "wpisz liczbe pierwsza:";
+	cin >> l1;
+	cout << "wpisz liczbe druga:";
+	cin >> l2;
+	cout << "wpisz liczbe trzecia:";
+	cin >> l3;
+	if (l1 < l2 && l2 < l3)
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l1<<"<" << l2 << "<" << l3;
+	else if (l2 < l1 && l1 < l3)
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l2 << "<" << l1<<"<"<< l3;
+	else if (l3 < l1 && l1 < l2)
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l3 << "<" << l1 << "<" << l2;
+	else if (l3 < l2 && l2 < l1)
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l3 << "<" <<l2 << "<" << l1;
+	else if (l2 < l3 && l3 < l1)
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l2 << "<" << l3 << "<" << l1;
+	else
+		cout << "oto liczby ulozone od najmniejszej do najwiekszej:" << l1 << "<" <<l3 << "<" << l2;
+	return 0;
+}
