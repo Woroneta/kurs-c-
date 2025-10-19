12cb18f711c3139705aec71e57b97c2f2a74df1d
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 10:58:02 +0200

    Create Zbiór zadań warunek.cpp

---
+++ Zbiór zadań warunek.cpp
@@ -0,0 +1,83 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	//===================DANE====================
+	double age, procent, section, l11, l12, l13;
+	int number;
+	//==================ZADANIE 1=================
+	cout << "podaj sówj wiek:" << endl;
+	cin >> age;
+	if (age >= 18) {
+		cout << " Masz prawo do głosowania" << endl;
+	}
+	else {
+		cout << "Nie masz prawa do głosowania" << endl;
+
+	}
+	//======================ZADANIE 2==================
+	cout << "=================================" << endl;
+	cout << "podaj wynik w procentach:" << endl;
+	cin >> procent;
+	if (procent <= 100 && procent >= 90) {
+		cout << "Ocena: celujący" << endl;
+	}
+	else if (procent < 90 && procent >= 75) {
+		cout << "Ocena: bardzo dobry" << endl;
+	}
+	else if (procent < 75 && procent >= 50) {
+		cout << "Ocena: dobry" << endl;
+	}
+	else if (procent < 50 && procent >= 00) {
+		cout << "Ocena: niedostateczny" << endl;
+	}
+	else {
+		cout << "niepoprawny procent" << endl;
+	}
+	//===================ZADANIE 3====================
+	cout << "=================================" << endl;
+	cout << "podaj liczbe:" << endl;
+	cin >> number;
+	if (number> 0) {
+		cout << "liczba jest dodatnia" << endl;
+	}
+	else if (number < 0) {
+		cout << "liczbna jest ujemna" << endl;
+	}
+	else {
+		cout << "liczba jest rowna zeru" << endl;
+	}
+	//================ZADANIE 4==================
+	cout << "=================================" << endl;
+	cout << "podaj liczbe:" << endl;
+	cin >> section;
+	if (section <= 200 && section >= 100) {
+		cout << "liczba znajduje sie w przedziale" << endl;
+	}
+	else {
+		cout << "liczba nie znajduje sie w przedziale" << endl;
+	}
+	//==============ZADANIE 5===================
+	cout << "================================" << endl;
+	cout << "podaj liczbe pierwsza: " << endl;
+	cin >> l11;
+	cout << "podaj liczeb druga:" << endl;
+	cin >> l12;
+	cout << "podaj liczbe trzecia:" << endl;
+	cin >> l13;
+	if (l11 > l12 && l11 > l13) {
+		cout << "liczba " << l11 << " jest najwieksza." << endl;
+	}
+	else if (l12 > l11 && l12 > l13) {
+		cout << "liczba " << l12 << "jest najwieksza." << endl;
+	}
+	else {
+		cout << "liczba " << l13 << " jest najwieksza." << endl;
+	}
+	cout << "Wszystek progamy zakonczyly dzialanie.";
+	return 0;
+}
