c8f0a78337d097a946e040386361ce6a4a05e3f9
Woroneta <WiktorWrobel816@gmail.com>
2025-10-19 12:30:52 +0200

    Create wpisz zero.cpp

---
+++ wpisz zero.cpp
@@ -0,0 +1,18 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+ 
+using namespace std;
+
+int main()
+{
+	int x;
+	do {
+		cout << "podaj licczbe:" << endl;
+		cin >> x;
+	} while (x != 0); {
+
+	}
+
+	return 0;
+}
