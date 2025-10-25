3bc9d790e0651f7045758c5d710722274fe488be
Woroneta <WiktorWrobel816@gmail.com>
2025-10-25 09:43:44 +0200

    Create czy pierwsza.cpp

---
+++ czy pierwsza.cpp
@@ -0,0 +1,29 @@
+#include<iostream>      
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+
+    if (n < 2) {
+        cout << "Liczba nie jest pierwsza";
+        return 0;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            cout << "Liczba nie jest pierwsza";
+            return 0;
+        }
+    }
+
+    cout << "Liczba jest pierwsza";
+    return 0;
+}
