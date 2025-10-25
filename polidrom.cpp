35dcc05bd16ed1f3dd2f54c9fc45966a5dd5b6ee
Woroneta <WiktorWrobel816@gmail.com>
2025-10-25 13:21:47 +0200

    Update and rename poldrom to polidrom.cpp

--- polidrom.cpp
+++ polidrom.cpp
@@ -9,17 +9,19 @@ int main() {
 	char name[20];
 	char name2[20];
 	cout << "wpisz zdanie:";
-	cin >> name;
-	for (int i = strlen(name) - 1; i >= 0; --i) {
-		name2 = name[i];
+	cin.getline(name, 20);
+
+	int len = strlen(name);
+	for (int i = 0; i < len; ++i) {
+		name2[i] = name[len - 1 - i];
 	}
+	name2[len] = '\0';
 	int cmp = strcmp(name, name2);
 	if (cmp == 0) {
 		cout << "wyraz jest palindromem.";
 	}
 	else {
-		cout << "wyaz nie jest palindromem.";
+		cout << "wyraz nie jest palindromem.";
 	}
 	return 0;
-}
 	
