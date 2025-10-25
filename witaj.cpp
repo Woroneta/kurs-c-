93eff359da00c6660ccdf770bab0cbb0b2a491c7
Woroneta <WiktorWrobel816@gmail.com>
2025-10-25 14:10:36 +0200

    Create witaj.cpp

---
+++ witaj.cpp
@@ -0,0 +1,15 @@
+#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+
+using namespace std;
+void hello() {
+	cout<<"witaj w swiecie c++";
+}
+
+int main() {
+	hello();
+
+	return 0;
+}
